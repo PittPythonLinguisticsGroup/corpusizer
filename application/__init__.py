171588fe47761fab70df61fa9a8afc1802ec1646
Mordeaux <michael.mordowanec@gmail.com>
2014-09-09 17:14:20 -0400

    some comments, comments are good for collaborating

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,19 @@
+###############################################################################
+##           Created by the Pitt Python Linguistics Group                     #
+##           https://github.com/PittPythonLinguisticsGroup                    #
+###############################################################################
+# Import from standard library here:
+
+
+# Import packages here:
 from flask import Flask, render_template
 
+# Create Flask app object:
 app = Flask(__name__)
 
+# Create new views here:
 @app.route('/')
 def index():
+    "This should return whatever we have decided the main page will be."
     return render_template('index.html')
+
